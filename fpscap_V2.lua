b39ee1e0e65549be4840be5e18b50cf22670fde6
Eisa Heinig <berlin30@me.com>
2023-02-23 22:48:12 +0100

    Add files via upload

---
+++ fpscap_V2.lua
@@ -0,0 +1,70 @@
+wait(30)
+print("FPSCAP")
+
+AccountExcludeFPS = "farmCharToExclude"
+print("FPS Account you want to Exclude:",AccountExcludeFPS)
+print("FPS Account found:",game.Players.LocalPlayer.Name)
+if game.Players.LocalPlayer.Name == AccountExcludeFPS then
+	print("Don't FPScap")
+else 
+	print("FPScap")
+	local UserInputService = game:GetService("UserInputService")
+	local RunService = game:GetService("RunService") 
+	RunService:Set3dRenderingEnabled(false)
+	setfpscap(30)
+	local WindowFocusReleasedFunction = function()
+		print("focus released")
+		RunService:Set3dRenderingEnabled(false)
+		setfpscap(30)
+		return
+	end 
+	local WindowFocusedFunction = function()
+		print("in focus")
+		RunService:Set3dRenderingEnabled(true)
+		setfpscap(60)
+		return
+	end 
+	local Initialize = function()
+		UserInputService.WindowFocusReleased:Connect(WindowFocusReleasedFunction)
+		UserInputService.WindowFocused:Connect(WindowFocusedFunction)
+		return
+	end
+	Initialize()
+	local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
+	local g = game
+	local w = g.Workspace
+	local l = g.Lighting
+	local t = w.Terrain
+	t.WaterWaveSize = 0
+	t.WaterWaveSpeed = 0
+	t.WaterReflectance = 0
+	t.WaterTransparency = 0
+	l.GlobalShadows = false
+	l.FogEnd = 9e9
+	l.Brightness = 0
+	settings().Rendering.QualityLevel = "Level01"
+	for i, v in pairs(g:GetDescendants()) do
+		if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+		elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+			v.Transparency = 1
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+		   v.Lifetime = NumberRange.new(0)
+		elseif v:IsA("Explosion") then
+			v.BlastPressure = 1
+			v.BlastRadius = 1
+		elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+			v.Enabled = false
+		elseif v:IsA("MeshPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+			v.TextureID = 10385902758728957
+		end
+	end
+	for i, e in pairs(l:GetChildren()) do
+		if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+			e.Enabled = false
+		end
+	end
+end
